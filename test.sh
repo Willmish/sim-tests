a1c62e631ca156729d2a869a2e6dfabc921cfe1b
Cindy Liu <hcindyl@google.com>
2022-01-04 16:18:48 -0800

    sim:tests: Update test script with the new option set
    Following
    github.com/renode/renode/commit/587f3e93fbccaa398905ee2eef194476d5ee29f8
    with a different run_tests.py argument set for the robot test.
    
    Change-Id: Iea22f9d30c7fd9806d9f4ebd5cb1b6713838a334

--- test.sh
+++ test.sh
@@ -7,21 +7,18 @@ fi
 
 set -u # Treat unset params as errors.
 
-RENODE_SRC_DIR="${ROOTDIR}/sim/renode"
 RENODE_DIR="${OUT}/host/renode"
-TESTS_FILE="${RENODE_SRC_DIR}/tests/tests.yaml"
 TESTS_RESULTS="${OUT}/renode_test_results"
 
-. "${RENODE_SRC_DIR}/tools/common.sh"
+source "${RENODE_DIR}/tests/common.sh"
 
-STTY_CONFIG=`stty -g 2>/dev/null`
-${PYTHON_RUNNER} -u "$(get_path "${RENODE_SRC_DIR}/tests/run_tests.py")" \
+STTY_CONFIG=$(stty -g 2>/dev/null)
+${PYTHON_RUNNER} -u "$(get_path "${RENODE_DIR}/tests/run_tests.py")" \
     --exclude "skip_${DETECTED_OS}" \
-    --properties-file "$(get_path "${RENODE_SRC_DIR}/output/properties.csproj")" \
     -r "$(get_path "${TESTS_RESULTS}")" \
-    -t "$(get_path "${TESTS_FILE}")" "$@" \
-    --variable PATH:"${ROOTDIR}" \
-    --robot-framework-remote-server-full-directory "${RENODE_DIR}" \
+    "$@" \
+    --robot-framework-remote-server-full-directory "${RENODE_DIR}/bin" \
+    --css-file "${RENODE_DIR}/tests/robot.css" \
     --show-log
 
 RESULT_CODE=$?
